f6f3f5f1873e538b3b9c8ab321914e13b88ae14a
MattDevGH <38076916+MattDevGH@users.noreply.github.com>
2019-10-13 14:43:43 +0100

    swapped fontawesome icons for gameicon icons

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { GiFaceToFace, GiCrossedSwords, GiLaurelCrown } from 'react-icons/gi'
 
 function Instructions () {
   return (
@@ -10,15 +10,15 @@ function Instructions () {
       <ol className='container-sm grid center-text battle-instructions'>
         <li>
           <h3 className='header-sm'>Enter two GitHub users</h3>
-          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
+          <GiFaceToFace className='bg-light' color='rgb(255, 191, 116)' size={140} />
         </li>
         <li>
           <h3 className='header-sm'>Battle</h3>
-          <FaFighterJet className='bg-light' color='#727272' size={140}/>
+          <GiCrossedSwords className='bg-light' color='#727272' size={140}/>
         </li>
         <li>
           <h3 className='header-sm'>See who wins!</h3>
-          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+          <GiLaurelCrown className='bg-light' color='rgb(255, 215, 0)' size={140} />
         </li>
       </ol>
     </div>
