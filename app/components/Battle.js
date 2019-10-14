1d2dc1e17f0a2926b8f025b73305afd10fe97dab
MattDevGH <38076916+MattDevGH@users.noreply.github.com>
2019-10-14 11:52:02 +0100

    created PlayerInput component in Battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GiFaceToFace, GiCrossedSwords, GiLaurelCrown } from 'react-icons/gi'
+import PropTypes from 'prop-types'
 
 function Instructions () {
   return (
@@ -23,6 +24,65 @@ function Instructions () {
       </ol>
     </div>
   )
+}
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.props.onSubmit(this.state.username)
+
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  render () {
+    return (
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input-light'
+            placeholder='github-username'
+            autoComplete='off'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className='btn btn-dark'
+            type='submit'
+            disabled={!this.state.username}>
+            Submit
+          </button>
+        </div>
+      </form>
+    )
+
+  }
+}
+
+PlayerInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
 }
 
 export default class Battle extends React.Component {
