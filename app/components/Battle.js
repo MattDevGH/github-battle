5fb35cee3cd05da074d6efafdc88a9312fc701dc
MattDevGH <38076916+MattDevGH@users.noreply.github.com>
2019-10-14 14:55:14 +0100

    started results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { GiFaceToFace, GiCrossedSwords, GiLaurelCrown, GiCancel } from 'react-icons/gi'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions () {
   return (
@@ -122,7 +123,8 @@ export default class Battle extends React.Component {
 
     this.state = {
       playerOne: null,
-      playerTwo: null
+      playerTwo: null,
+      battle: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -142,7 +144,11 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state
+    const { playerOne, playerTwo, battle } = this.state
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />
+    }
     return (
       <React.Fragment>
         <Instructions />
@@ -176,6 +182,15 @@ export default class Battle extends React.Component {
               />
             }
           </div>
+          {playerOne && playerTwo && (
+            <button 
+              className='btn btn-dark btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle!
+            </button>
+          )}
+          
         </div>
       </React.Fragment>
     )
